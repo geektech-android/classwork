ab8f043b09111f93a410d7f53862bb4fad1c9285
Kudaybergen <tuleubekov.kas@gmail.com>
2018-12-10 15:43:15 +0600

    Fix minor

--- app/src/main/java/com/akai/geektech/classwork/MainActivity.java
+++ app/src/main/java/com/akai/geektech/classwork/MainActivity.java
@@ -1,7 +1,6 @@
 package com.akai.geektech.classwork;
 
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
