99dfd53257181913f0b0f0347cf926c426c13475
Kudaybergen <tuleubekov.kas@gmail.com>
2018-12-10 14:28:04 +0600

    Lesson 1

--- app/src/main/java/com/akai/geektech/classwork/MainActivity.java
+++ app/src/main/java/com/akai/geektech/classwork/MainActivity.java
@@ -1,13 +1,75 @@
 package com.akai.geektech.classwork;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = "MainActivity";
+    private DrawerLayout mDrawerlayout;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        mDrawerlayout = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mDrawerlayout, mToolbar, R.string.drawer_open, R.string.drawer_close
+        );
+        mDrawerlayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navView = findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Class fragmentClass = null;
+        switch (menuItem.getItemId()) {
+            case R.id.item1:
+                fragmentClass = FirstFragment.class;
+                log("item 1 clicked");
+                break;
+            case R.id.item2:
+                fragmentClass = SecondFragment.class;
+                log("item 2 clicked");
+                break;
+        }
+
+        Fragment fragment = null;
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction()
+                .replace(R.id.container_fragment, fragment)
+                .commit();
+
+        mDrawerlayout.closeDrawers();
+        return true;
+    }
+
+    public static void log(String message) {
+        Log.d(TAG, message);
     }
 }
