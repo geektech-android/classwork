539fc5fc36285a73ad3c01091318ff83bc69f9c1
Kudaybergen <tuleubekov.kas@gmail.com>
2018-12-10 15:10:14 +0600

    Add view pager

--- app/src/main/java/com/akai/geektech/classwork/MainActivity.java
+++ app/src/main/java/com/akai/geektech/classwork/MainActivity.java
@@ -1,13 +1,32 @@
 package com.akai.geektech.classwork;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private ViewPager mPager;
+    private PagerAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPager = findViewById(R.id.view_pager);
+        mAdapter = new PagerAdapter(getSupportFragmentManager(), getFragments());
+        mPager.setAdapter(mAdapter);
+    }
+
+    private List<Fragment> getFragments() {
+        List<Fragment> list = new ArrayList<>();
+        list.add(FirstFragment.getInstance());
+        list.add(SecondFragment.getInstance());
+        list.add(ThirdFragment.getInstance());
+        return list;
     }
 }
---
+++ app/src/main/java/com/akai/geektech/classwork/PagerAdapter.java
@@ -0,0 +1,26 @@
+package com.akai.geektech.classwork;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.List;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    private List<Fragment> mFragments;
+
+    public PagerAdapter(FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+        mFragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return mFragments.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+}
